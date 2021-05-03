3b29eee6706f84347db1b945cc21c7f813a5f228
TheDariusz <thedariusz@gmail.com>
2021-05-03 21:14:23 +0200

    project setup

---
+++ src/main/java/pl/coderslab/warnme/WarnMeApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.warnme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarnMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarnMeApplication.class, args);
+	}
+
+}
