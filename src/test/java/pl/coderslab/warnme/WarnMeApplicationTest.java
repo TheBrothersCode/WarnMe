534d8076dea63253e69817811502a44e4ed2c8b2
Dariusz Szczepański <thedariusz@gmail.com>
2021-05-04 20:17:55 +0200

    #5 Setup project structure  (#15)

---
+++ src/test/java/pl/coderslab/warnme/WarnMeApplicationTest.java
@@ -0,0 +1,22 @@
+package pl.coderslab.warnme;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WarnMeApplicationTest {
+
+    WarnMeApplication app = new WarnMeApplication();
+
+    @Test
+    void shouldSayHelloToAdam() {
+        //given
+        final String adam = "Adam";
+
+        //when
+        final String actual = app.sayHello(adam);
+
+        //then
+        final String expected = "Hi Adam";
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
